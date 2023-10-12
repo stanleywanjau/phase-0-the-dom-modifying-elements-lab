8e44b100f7efa90f344972604afb41bd04c41bbf
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-12 15:48:48 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+gi
+let main = document.querySelector('#main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+
+newHeader.setAttribute("id","victory");
+let victory = document.getElementById('victory')
+victory.innerHTML = 'stanley is the champion';
